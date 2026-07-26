b6190025d36262d4ec917c13f4fb0ec603a88223
Gayatri Randhe <gayatrirandhe1000@gmail.com>
2026-07-26 19:40:41 +0530

    Delete Question1.py

--- Question1.py
+++
@@ -1,8 +0,0 @@
-#Largest of 2 numbers
-num1= int(input("Enter first number: "))
-num2= int(input("Enter second number: "))
-
-if num1 > num2:
-    print(f"{num1} is the largest number.")
-else:
-    print(f"{num2} is the largest number.")
